50aa5b851ed4f258144bb8f525bd6218c975f9ee
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-25 13:03:24 +0530

    print the display something

--- SimpleFormate.java
+++ SimpleFormate.java
@@ -1,7 +1,7 @@
 public class SimpleFormate {
 
     public static void main(String args[]) {
-    
+        System.out.print(" Hii Roshan");
         System.out.println(" Dsa with Alpha Batch");
     }
 }
