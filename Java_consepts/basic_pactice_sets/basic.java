eead5e752459168740effc4cc1cab58bddd2336d
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-26 09:35:17 +0530

    print the all the variable type .

---
+++ Java_consepts/basic_pactice_sets/basic.java
@@ -0,0 +1,24 @@
+package Java_consepts.basic_pactice_sets;
+
+public class basic {
+    public static void main(String args[]) {
+        int a = 10;
+        byte b = 20;
+        float f = 2.23f;
+        double d = 3.45;
+        String name = "Mr. Been";
+        long l = 2332;
+        short ll = 222;
+        char c = 'c';
+        // print the all the variable values
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(name);
+        System.out.println(l);
+        System.out.println(ll);
+        System.out.println(c);
+
+    }
+}
