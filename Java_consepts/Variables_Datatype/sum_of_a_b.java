5c3d0f0b842dc174ab36fc268ad31a76d9eaecd0
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-26 09:58:15 +0530

    print the sum of a and b

---
+++ Java_consepts/Variables_Datatype/sum_of_a_b.java
@@ -0,0 +1,12 @@
+package Java_consepts.Variables_Datatype;
+
+public class sum_of_a_b {
+    public static void main(String args[]) {
+        int a = 100;
+        int b = 200;
+        int sum = 0;
+        sum = a + b;
+        System.out.println(sum);
+
+    }
+}
