5fb3fe8c035af47810e738198baad25bede502f9
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-26 08:55:34 +0530

    print the patters using System.out.print() function

--- Java_consepts/basic_pactice_sets/PrintPatters.java
+++ Java_consepts/basic_pactice_sets/PrintPatters.java
@@ -2,6 +2,9 @@ package Java_consepts.basic_pactice_sets;
 
 public class PrintPatters {
     public static void main(String args[]) {
-
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
