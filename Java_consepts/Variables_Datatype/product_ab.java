887d02fcb02a72eedaff01e3319b4f78440790fa
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-27 12:15:10 +0530

    product of first and second number

---
+++ Java_consepts/Variables_Datatype/product_ab.java
@@ -0,0 +1,15 @@
+package Java_consepts.Variables_Datatype;
+
+import java.util.*;
+
+public class product_ab {
+    public static void main(String args[]) {
+        // take the two input fist and second number and calculate the product
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        int product = first + second;
+        System.out.println("Product of two no. : " + product);
+    }
+
+}
