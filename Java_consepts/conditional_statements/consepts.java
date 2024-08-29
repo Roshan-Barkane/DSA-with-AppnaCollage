3567d466e3ea9d21fff052a6815d8cccd3aefc8e
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-29 23:06:00 +0530

    concepts are conditional statements

---
+++ Java_consepts/conditional_statements/consepts.java
@@ -0,0 +1,14 @@
+package Java_consepts.conditional_statements;
+
+public class consepts {
+    // Condition Statements
+    /*
+     * Type of condition statements
+     * ---------> if statements
+     * ---------> if else statements
+     * ---------> else if statements
+     * ---------> nested statements
+     * ---------> ternary statements
+     * ---------> switch statements
+     */
+}
