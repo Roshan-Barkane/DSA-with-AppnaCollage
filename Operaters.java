1aa9c42b1879db13d95ecd791ad1b6d368267760
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-08-29 21:50:54 +0530

    write the arithmetic operation code.

---
+++ Operaters.java
@@ -0,0 +1,26 @@
+class Operaters {
+    public static void main(String args[]) {
+        /*
+         * All the operators in java
+         * 1. Arithmetic operators
+         * 2. Relationship operators
+         * 3. Logical operators
+         * 4. Bitwise operators
+         * 5. Assignment operators
+         * 
+         */
+        // Arithmetic operators
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Add of two value " + (10 + 5));
+        System.out.println("sub of two value " + (10 - 5));
+        System.out.println("multi of two value " + (10 * 5));
+        System.out.println("div of two value " + (10 / 5));
+        System.out.println("modulo of two value " + (10 % 5));
+        System.out.println("before increase then print a " + (++a));
+        System.out.println("print the a then increase " + (a++));
+        System.out.println("before decrease then print b " + (--b));
+        System.out.println("print the b then decrease " + (b--));
+    }
+}
