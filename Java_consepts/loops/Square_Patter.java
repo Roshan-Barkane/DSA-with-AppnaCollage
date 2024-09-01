97890dbe7c8cf66d3e8636be3e7e315ec4015039
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-01 18:47:36 +0530

    print the square patter 4*4 .

---
+++ Java_consepts/loops/Square_Patter.java
@@ -0,0 +1,13 @@
+package Java_consepts.loops;
+
+public class Square_Patter {
+    public static void main(String args[]) {
+        // Print square patter with for loop
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 0; j <= 3; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
