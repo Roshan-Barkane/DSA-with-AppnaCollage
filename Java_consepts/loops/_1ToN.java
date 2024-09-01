09441def646f664382b28f5eda7ab8574cdb9a3d
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-01 18:22:55 +0530

    print the 1 to N that N is take the input

---
+++ Java_consepts/loops/_1ToN.java
@@ -0,0 +1,16 @@
+package Java_consepts.loops;
+
+import java.util.*;
+
+public class _1ToN {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        while (count <= n) {
+            System.out.println("value of n : " + count);
+            count++;
+        }
+    }
+
+}
