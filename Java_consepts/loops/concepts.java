a1cb55ac9ef4fc960ef86797faa40b64505612a5
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-01 08:01:27 +0530

    learn concept of loop and explore the type and syntax too .

---
+++ Java_consepts/loops/concepts.java
@@ -0,0 +1,42 @@
+package Java_consepts.loops;
+
+public class concepts {
+    public static void main(String args[]) {
+        // loops : - if we should some task are doing again in again.
+        /*
+         * loops are two types exit and entry control loops
+         * 
+         * exit control loop :- first time task are execute the before go to next
+         * statement condition are check if condition are true then task are again
+         * execute if condition are false then go to next statement.
+         * type ---> do-while loop.
+         * 
+         * ## syntax for do-while loop
+         * do{
+         * ---------
+         * ---------
+         * ---------
+         * }while(condition);
+         * 
+         * entry control loop :- execute the task before we cen check the condition are
+         * true are not if condition are true then task are execute and if condition are
+         * false then skip the task then execute the next statement .
+         * type ---> white loop , for loop.
+         * 
+         * ## syntax for while loop
+         * while(condition){
+         * ------------
+         * ------------
+         * ------------
+         * }
+         * 
+         * ## syntax for for loop
+         * for(initialize; condition;inc/dec){
+         * -----------
+         * -----------
+         * -----------
+         * }
+         * 
+         */
+    }
+}
