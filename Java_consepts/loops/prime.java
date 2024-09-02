b1cae561f61ff8cf7a805f03acd2963ff0261d8f
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-02 14:58:17 +0530

    take the any no. then ckeck the no. is prime are not if no, is prime print no. is prime otherwise no. is not prime .

---
+++ Java_consepts/loops/prime.java
@@ -0,0 +1,21 @@
+package Java_consepts.loops;
+
+import java.util.*;
+
+public class prime {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime == true) {
+            System.out.println("No. is prime");
+        } else {
+            System.out.println("No. is not prime");
+        }
+    }
+}
