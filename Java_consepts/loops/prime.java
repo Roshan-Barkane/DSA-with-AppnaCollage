57290c5f9818ef311481eceadac11efd40120157
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-02 15:01:48 +0530

    write the code n is prime or not.

--- Java_consepts/loops/prime.java
+++ Java_consepts/loops/prime.java
@@ -6,16 +6,22 @@ public class prime {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        boolean isPrime = true;
-        for (int i = 2; i <= n - 1; i++) {
-            if (n % i == 0) {
-                isPrime = false;
-            }
-        }
-        if (isPrime == true) {
+
+        if (n == 2) {
             System.out.println("No. is prime");
         } else {
-            System.out.println("No. is not prime");
+            boolean isPrime = true;
+            for (int i = 2; i <= n - 1; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime == true) {
+                System.out.println("No. is prime");
+            } else {
+                System.out.println("No. is not prime");
+            }
         }
+
     }
 }
