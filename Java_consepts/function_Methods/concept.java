bf91f5913c5f0da1e90adcf53eb294f623a0e975
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-04 19:05:00 +0530

    make a simple methods perform the to sentence .

---
+++ Java_consepts/function_Methods/concept.java
@@ -0,0 +1,29 @@
+package Java_consepts.function_Methods;
+
+public class concept {
+    /*
+     * Function and Methods --> function is a set of instruction perform the special
+     * task and that is reduce the line of code in a program.
+     * syntax :-
+     * return type function_name(arguments){
+     * ------------
+     * ------------- body of the function
+     * -------------
+     * return statement;
+     * }
+     */
+    // for print the hello programming and i am coming soon.
+    static void HelloProg() {
+        System.out.println("Abbu aa rhe he ...");
+        System.out.println("I am coming soon");
+    }
+
+    public static void main(String args[]) {
+        // print the hello programming with function
+        HelloProg();
+        HelloProg();
+        HelloProg();
+        HelloProg();
+
+    }
+}
