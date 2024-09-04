028645be6ba6dcc150ac9f6e8a0a93bc89a15afc
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-04 11:26:27 +0530

    concepts of nested loop and how to use with patterns .

---
+++ Java_consepts/Patters_part1/i_time.java
@@ -0,0 +1,14 @@
+package Java_consepts.Patters_part1;
+
+public class i_time {
+    public static void main(String args[]) {
+        /*
+         * concept of patters using nested loop
+         * each patter need three steps
+         * 1. no.of line ---> outer loop 1 to no. of line
+         * 2. how many star print inner loop go to 1 to no. of star in line
+         * 3. how to print us.
+         *
+         */
+    }
+}
