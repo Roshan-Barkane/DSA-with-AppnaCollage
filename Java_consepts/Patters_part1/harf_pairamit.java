6e8df65c77d9628bd5d1649ffdcc522e50f8b0be
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-04 11:48:39 +0530

    print the half _ pyramid patter .

---
+++ Java_consepts/Patters_part1/harf_pairamit.java
@@ -0,0 +1,12 @@
+package Java_consepts.Patters_part1;
+
+public class harf_pairamit {
+    public static void main(String args[]) {
+        for (int line = 1; line <= 5; line++) {
+            for (int star = 1; star <= line; star++) {
+                System.out.print(star);
+            }
+            System.out.println("");
+        }
+    }
+}
