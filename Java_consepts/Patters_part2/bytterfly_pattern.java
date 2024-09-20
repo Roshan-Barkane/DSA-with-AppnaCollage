b33ef6fc943e3855e815bc68eac445690c2f0cef
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-20 08:10:21 +0530

    write the code of butterfly patter print .

---
+++ Java_consepts/Patters_part2/bytterfly_pattern.java
@@ -0,0 +1,46 @@
+package Java_consepts.Patters_part2;
+
+public class bytterfly_pattern {
+    public static void bytterflyPatterm(int n) {
+        // 1st half butterfly
+        for (int i = 1; i <= n; i++) {
+            // for inner loop for star- i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // for inner loop for space - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+
+            }
+
+            // for inner loop for star - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2st half butterfly
+        for (int i = n; i >= 1; i--) {
+            // for inner loop for star- i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // for inner loop for space - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+
+            }
+
+            // for inner loop for star - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        bytterflyPatterm(4);
+    }
+}
