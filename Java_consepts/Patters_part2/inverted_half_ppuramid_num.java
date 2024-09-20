a546861242f9102f537ebb53b195f7913449f92e
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-20 05:38:51 +0530

    print inverted half pyramid with number .

--- Java_consepts/Patters_part2/inverted_half_ppuramid_num.java
+++ Java_consepts/Patters_part2/inverted_half_ppuramid_num.java
@@ -1,22 +1,22 @@
 package Java_consepts.Patters_part2;
 
-public class inverted_half_pyramid {
+public class inverted_half_ppuramid_num {
     public static void InvertedHalfPyramid(int n) {
         // for inner loop
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n - 1; i++) {
             // for print space
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                System.out.print(j);
             }
             // for print star
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print("   ");
             }
             System.out.println();
         }
     }
 
     public static void main(String args[]) {
-        InvertedHalfPyramid(7);
+        InvertedHalfPyramid(5);
     }
 }
