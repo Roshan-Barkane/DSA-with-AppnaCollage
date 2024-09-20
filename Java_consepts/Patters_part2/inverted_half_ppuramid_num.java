82eab0c9d25076dd83e05cc0aaa513f06af549de
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-20 05:46:43 +0530

    second statigy to print inverted half pyramid with number .

--- Java_consepts/Patters_part2/inverted_half_ppuramid_num.java
+++ Java_consepts/Patters_part2/inverted_half_ppuramid_num.java
@@ -1,17 +1,31 @@
 package Java_consepts.Patters_part2;
 
 public class inverted_half_ppuramid_num {
-    public static void InvertedHalfPyramid(int n) {
+    // for first way to print inverter half pyramid with number
+    public static void InvertedHalfPyramid1(int n) {
         // for inner loop
         for (int i = 0; i <= n - 1; i++) {
-            // for print space
+            // for print number
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(j);
             }
-            // for print star
+            // for print space
             for (int j = 1; j <= i; j++) {
                 System.out.print("   ");
             }
+            System.out.println();
+        }
+    }
+
+    // for second way to print inverter half pyramid with number
+    public static void InvertedHalfPyramid(int n) {
+        // for inner loop
+        for (int i = 1; i <= n; i++) {
+            // for print number
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+
             System.out.println();
         }
     }
