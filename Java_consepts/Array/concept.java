10da6874d5438de0ad28ef263989b65ea7ed9b67
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-09-23 16:32:09 +0530

    function and methods are done !

---
+++ Java_consepts/Array/concept.java
@@ -0,0 +1,18 @@
+package Java_consepts.Array;
+
+public class concept {
+    public static void main(String ar[]) {
+        /*
+         * Array - array is list of element of the some type placed in a contiguous
+         * memory allocation
+         * => Operation - create,input,output,update etc.
+         */
+        // create the array
+        int num[] = new int[10]; // type 1
+        String name[] = { "Roshan", "Lokesh", "Shubham", "Aanad", "Navin" }; // type 2
+        int age[] = { 20, 13, 48, 39, 24, 22 };
+
+        // input and output in arrays
+
+    }
+}
