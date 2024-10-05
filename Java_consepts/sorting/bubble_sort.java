d9e87d690968042c689a56c0c6fa79ef12bf2973
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-10-05 15:30:29 +0530

    write the bubble sort algorithm increasing  order .

---
+++ Java_consepts/sorting/bubble_sort.java
@@ -0,0 +1,26 @@
+package Java_consepts.sorting;
+
+public class bubble_sort {
+    public static void bubbleSort(int a[]) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] >= a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(" " + a[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        int a[] = { 12, 43, 23, 51, 19, 8 };
+        bubbleSort(a);
+    }
+}
