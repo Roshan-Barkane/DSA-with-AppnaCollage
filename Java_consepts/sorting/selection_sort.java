6ae469f05b15c54a6d1b55401a0021aa95e74d79
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-10-07 08:30:08 +0530

    write the code selection sort in array element

---
+++ Java_consepts/sorting/selection_sort.java
@@ -0,0 +1,31 @@
+package Java_consepts.sorting;
+
+public class selection_sort {
+    // array are sort decrease order
+    public static void selectionSort(int a[]) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[minPos] < a[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = a[minPos];
+            a[minPos] = a[i];
+            a[i] = temp;
+        }
+    }
+
+    // print the array element
+    public static void printArray(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(" " + a[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        int a[] = { 16, 4, 15, 1, 2, 13 };
+        selectionSort(a);
+        printArray(a);
+    }
+}
