fccbde9ec33227e85154e6da2752b3a13bfd58d5
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-10-10 14:38:28 +0530

    found the element  is present the 2d array or not.

--- Java_consepts/Two_D_Array/concept.java
+++ Java_consepts/Two_D_Array/concept.java
@@ -3,6 +3,18 @@ package Java_consepts.Two_D_Array;
 import java.util.*;
 
 class consept {
+    // search the element
+    public static boolean search(int arr[][], int key) {
+        int n = arr.length, m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == key) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
     public static void main(String arg[]) {
         int arr[][] = new int[3][3];
@@ -22,6 +34,11 @@ class consept {
             }
             System.out.println();
         }
-
+        boolean check = search(arr, 5);
+        if (check = true) {
+            System.out.println("Found the elements");
+        } else {
+            System.out.println("Not Found the element");
+        }
     }
 }
