5d81385a7a065cc93cac787a6aa32e97a8b884f4
Roshan-Barkane <roshanbaekhane987@gmail.com>
2024-10-20 16:16:16 +0530

    concept of the string and get the input print the input as a string.

---
+++ Java_consepts/strings/consept.java
@@ -0,0 +1,41 @@
+package Java_consepts.strings;
+
+import java.util.Scanner;
+
+public class consept {
+    public static void main(String args[]) {
+        /*
+         * if you are stored the letter simply use character array but if you store as a
+         * word sentence paragraph we can need to new structure called String
+         * 
+         * ---- char arr[] = {'a','b','c','d'};
+         * ---- String str = "abcd"; --------------------- declare string
+         * ---- String str2 = new Sting("abcde"); -------- declare string
+         * Key-points : java has String is immutable means string does't change and any
+         * type of data integer float special character store only double quotation
+         * example---- "123 name @£$"
+         */
+
+        // String str = "abcd"; // declare String
+
+        // take the input in the String
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+
+        // his function only get one word console and the variable in name
+        // name = sc.next();
+
+        // his function get all the string with space
+        // name = sc.nextLine();
+
+        // print the string value
+        // System.out.println(name);
+
+        // get the length of the string
+        // System.out.println(name);
+        // System.out.println(name.length());
+
+        // Concatenation in the string
+
+    }
+}
